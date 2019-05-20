71c43c3cb9cdfd7ebaf4e64466bd7a07c46c6c24
quanganh9x <who@is.quanganh9x>
2019-05-20 13:46:29 +0700

    Login with Identity & JWT bearer

---
+++ Taurus/Models/Formats/APIResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taurus.Models.Formats
+{
+    public class APIResponse
+    {
+        public APIStatus Status { get; set; }
+        public object Data { get; set; }
+    }
+
+    public enum APIStatus
+    {
+        Success = 0,
+        Failed = 1
+    }
+}
