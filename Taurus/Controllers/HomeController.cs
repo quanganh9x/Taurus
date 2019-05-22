93d9c90abbcee3c649a6bd2c4e9bc342d53c1f58
quanganh9x <quanganh9x>
2019-05-22 10:55:12 +0700

    Small refactor

--- Taurus/Controllers/HomeController.cs
+++ Taurus/Controllers/HomeController.cs
@@ -56,7 +56,7 @@ namespace Taurus.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ViewError { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Taurus/Models/Exceptions/ViewError.cs
+++ Taurus/Models/Exceptions/ViewError.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Taurus.Models
 {
-    public class ErrorViewModel
+    public class ViewError
     {
         public string RequestId { get; set; }
 
